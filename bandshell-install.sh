156eace5d2945166e3f23697497ac3effd848fcb
Michael Holt <mholt@reallifechurch.org>
2015-03-10 12:25:08 -0700

    Initial version of Bandshell Installer

---
+++ bandshell-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Requires Sudo
+if [ "$(id -u)" != "0" ]; then
+echo "This script must be run as root" 1>&2
+exit 1
+fi
+
+# Install packages we need (build-essential is temporary)
+apt-get -y install xserver-xorg xserver-xorg-video-all \
+	chromium unclutter ifplugd xinit blackbox \
+	ruby1.9.1-full build-essential \
+	vim screen git-core openssh-server \
+	firmware-linux-nonfree
+
+# Install Bandshell
+cd /tmp
+git clone git://github.com/concerto/bandshell.git
+cd bandshell
+gem build bandshell.gemspec
+gem install *.gem –no-ri –no-rdoc
+cd /
+rm -rf /tmp/bandshell
+
+apt-get -y purge build-essential
+apt-get -y autoremove
