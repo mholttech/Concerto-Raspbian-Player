c6404942f0f4a0753cf9e0fb79cb535deb5eb603
Michael Holt <mholt@reallifechurch.org>
2015-03-10 12:28:41 -0700

    Removed sudo from commands

--- kiosk-packages.sh
+++ kiosk-packages.sh
@@ -7,7 +7,7 @@ echo "This script must be run as root" 1>&2
 exit 1
 fi
 
-sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install matchbox chromium x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
-sudo reboot
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y install matchbox chromium x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
+reboot
